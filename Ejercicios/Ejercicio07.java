4bc05d8165a11a3613db86c48ab684f54481a74b
Markets <marcosf18s26@gmail.com>
2026-05-13 18:50:48 +0200

    Corregido

--- Ejercicios/Ejercicio07.java
+++ Ejercicios/Ejercicio07.java
@@ -1,36 +1,45 @@
-
-import Ejercicios.Entrada;
+package Ejercicios;
 
 public class Ejercicio07 {
-        public static void main(String[] args) {
-              int num1 =0;
-              int num2 =0;
-              int operacion = 0;
-              
-              System.out.println("Introduce el primer numero");
-              num1 = Entrada.leerEntero();
-              System.out.println("Introduce el segundo numero");
-              num2 = Entrada.leerEntero();
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int operacion;
 
-            switch (operacion) {
-                case 1 -> {
-                    int suma= num1+num2;
-                    System.out.println("La suma de " + num1 + " + " + num2 + " es igual a " + suma);
-                }
-                case 2 -> {
-                    int resta = num1 - num2;
-                    System.out.println("La resta de " + num1 + " + " + num2 + " es igual a " + resta);
-                }
-                case 3 -> {
-                    int multi = num1 * num2;
-                    System.out.println("La multiplicación de " + num1 + " + " + num2 + " es igual a " + multi);
-                }
-                case 4 -> {
-                    int dividir = num1 / num2;
-                    System.out.println("La division de " + num1 + " + " + num2 + " es igual a " + dividir);
-                }
-                default -> System.out.println("El numero no corresponde a ninguna opción");
+        System.out.println("Introduce el primer numero");
+        num1 = Entrada.leerEntero();
+        System.out.println("Introduce el segundo numero");
+        num2 = Entrada.leerEntero();
+
+        System.out.println("Elige una operacion:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        operacion = Entrada.leerEntero();
+
+        switch (operacion) {
+            case 1 -> {
+                int suma = num1 + num2;
+                System.out.println("La suma de " + num1 + " + " + num2 + " es igual a " + suma);
             }
-
+            case 2 -> {
+                int resta = num1 - num2;
+                System.out.println("La resta de " + num1 + " - " + num2 + " es igual a " + resta);
+            }
+            case 3 -> {
+                int multiplicacion = num1 * num2;
+                System.out.println("La multiplicacion de " + num1 + " * " + num2 + " es igual a " + multiplicacion);
+            }
+            case 4 -> {
+                if (num2 == 0) {
+                    System.out.println("No se puede dividir entre 0");
+                } else {
+                    int division = num1 / num2;
+                    System.out.println("La division de " + num1 + " / " + num2 + " es igual a " + division);
+                }
+            }
+            default -> System.out.println("El numero no corresponde a ninguna opcion");
         }
+    }
 }
